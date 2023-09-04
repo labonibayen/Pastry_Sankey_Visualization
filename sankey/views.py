f9140ef74ef475e2bfa3985888ee02dce7e99c9e
labonibayen <lbayen@umich.edu>
2023-09-04 13:20:14 +0000

    added html

--- sankey/views.py
+++ sankey/views.py
@@ -1,3 +1,16 @@
+from __future__ import unicode_literals
+from .models import DataPoint
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.forms.models import model_to_dict
+import json
+import csv
+import codecs
 from django.shortcuts import render
+from django.core import serializers
 
-# Create your views here.
+
+
+def sankeydiagram(request):
+	rows = serializers.serialize("json", DataPoint.objects.filter(in_sankey = True))
+	return render(request, 'sankeyvis/index.html', {'rows':rows})
